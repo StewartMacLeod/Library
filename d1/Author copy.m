9ccfd2a07473ccf5f06ea1a48093a34756fe5b02
StewartMacLeod <Stewart.P.MacLeod@gmail.com>
2015-05-30 16:06:28 -0700

    updated all comments and cleaned up files.

---
+++ d1/Author copy.m
@@ -0,0 +1,22 @@
+//
+//  Author.m
+//  myLibrary Application
+//
+//  Created by Stewart MacLeod on 5/28/15.
+//  Copyright (c) 2015 Stewart MacLeod. All rights reserved.
+//
+//  This is the implementation file for Author but currently it
+//  has no methods and
+//  relies on the default setters and getters of properties.
+//
+
+#import "Author.h"
+
+@implementation Author
+
+@end
+
+
+
+
+
---
+++ d1/Book copy.m
@@ -0,0 +1,16 @@
+//
+//  Book.m
+//  myLibrary Application
+//
+//  Created by Stewart MacLeod on 5/28/15.
+//  Copyright (c) 2015 Stewart MacLeod. All rights reserved.
+//
+//  This class contains the most important information about
+//  each Book and is the primary model of the MVC.
+//
+
+#import "Book.h"
+
+@implementation Book
+
+@end
